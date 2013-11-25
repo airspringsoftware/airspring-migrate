6a2c286cc52209f0d3b8a54b0f946125a8744bad
chriswininger <cwininger@airspringsoftware.com>
2013-11-25 09:19:10 -0500

    Created bin with mongo-migrate and put code for dealing with command line calls to the module here, leaving module code in index.js. Added bin key package.json to create a mongo-migrate link in /usr/local/bin for executing from command line; Changed mongodb to a dependency from a devDependency as the project will not run without it; Removed the need to pass -runmm when calling from the command line (now you just call mongo-migrate); Added a function which installs the mongodb module into the migration folder on create if it is not already there because the template crated for the migrations has a require('mongodb') statement which will fail if this module is not installed at the location;

---
+++ bin/mongo-migrate.js
@@ -0,0 +1,92 @@
+#!/usr/bin/env node
+
+var migrate = require('../');
+
+/**
+ * Arguments.
+ */
+var args = process.argv.slice(2);
+
+/**
+ * Option defaults.
+ */
+var options = { args: [] };
+
+/**
+ * Usage information.
+ */
+var usage = [
+    ''
+    , '  Usage: migrate [options] [command]'
+    , ''
+    , '  Options:'
+    , ''
+    , '     -runmm, --runMongoMigrate   Run the migration from the command line',
+    , '     -c, --chdir <path>    		change the working directory'
+    , '     -cfg, --config <path> 		DB config file name'
+    , '     -dbn, --dbPropName <string> Property name for database connection in config file'
+    , ''
+    , '  Commands:'
+    , ''
+    , '     down   [name]    migrate down till given migration'
+    , '     up     [name]    migrate up till given migration (the default command)'
+    , '     create [title]   create a new migration file with optional [title]'
+    , ''
+].join('\n');
+
+
+/**
+ * require an argument
+ * @returns {*}
+ */
+function required() {
+    if (args.length) return args.shift();
+    abort(arg + ' requires an argument');
+}
+
+/**
+ * abort with a message
+ * @param msg
+ */
+function abort(msg) {
+    console.error('  %s', msg);
+    process.exit(1);
+}
+
+
+var runMongoMigrateIdx = args.indexOf('--runMongoMigrate');
+if (runMongoMigrateIdx > -1) args.splice(runMongoMigrateIdx, 1);
+
+// parse arguments
+var arg;
+while (args.length) {
+    arg = args.shift();
+    switch (arg) {
+        case '-h':
+        case '--help':
+        case 'help':
+            console.log(usage);
+            process.exit();
+            break;
+        case '-c':
+        case '--chdir':
+            migrate.changeWorkingDirectory(required());
+            break;
+        case '-cfg':
+        case '--config':
+            migrate.setConfigFilename(required());
+            break;
+        case '-dbn':
+        case '--dbPropName':
+            migrate.setConfigFileProp(required());
+            break;
+        default:
+            if (options.command) {
+                options.args.push(arg);
+            } else {
+                options.command = arg;
+            }
+    }
+}
+
+migrate.run(options);
